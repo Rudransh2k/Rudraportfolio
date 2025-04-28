5598c469dc45d098002c04b38fafca2966d9f6ae
Rudransh2k <118921370+Rudransh2k@users.noreply.github.com>
2025-04-28 20:07:36 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+// Scroll to Top Button
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+
+window.onscroll = function() {
+  if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+    scrollTopBtn.style.display = "block";
+  } else {
+    scrollTopBtn.style.display = "none";
+  }
+};
+
+scrollTopBtn.addEventListener("click", function() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
